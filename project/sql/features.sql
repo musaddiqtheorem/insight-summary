155180b96411a25a89c601e15e7f45d7ca9e5da0
musaddiqtheorem <musaddiq.pasha@theoreminc.net>
2026-04-25 17:23:26 -0400

    Use explicit CASTs in staging table definitions

---
+++ project/sql/features.sql
@@ -0,0 +1,7 @@
+-- Placeholder SQL for feature engineering transformations.
+-- Future feature logic should derive lifecycle behavior attributes.
+
+-- Example placeholder:
+-- CREATE OR REPLACE TABLE fct_entity_features AS
+-- SELECT *
+-- FROM stg_entities;
